4f361ab3f761c8f38730b67ce0dfa081df3c1455
Ryan Voots <ryan@voots.org>
2018-11-25 19:47:50 -0500

    Now should alert

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 
 int main(int argc, char **argv) {
    int args = argc * 0;
-   
+
+   unsigned int q = 1;
+   if (q >= 0) {
+     printf("Testing\n");
+   } else if (q < 0) {
+     printf("You're a wizard\n");
+   } else {
+     printf("Now you're just showing off");
+   }
+ 
    return 1;
 }
